df84690b2f8fcd3d2dd65f4ff023c317a851b202
gargsuraj12 <garg.suraj12@gmail.com>
2018-09-21 12:56:30 +0530

    Working serverTCP.cpp

--- serverTCP.cpp
+++ serverTCP.cpp
@@ -1,10 +1,5 @@
-//#include <netdb.h> 
-//#include <netinet/in.h> 
 #include <stdlib.h> 
 #include <string.h> 
-//#include <sys/socket.h> 
-//#include <sys/types.h>
-//#include <arpa/inet.h>
 #include <unistd.h>
 #include <iostream>
 #include <string>
@@ -19,7 +14,7 @@
  using namespace std; 
   
 // Function designed for chat between client and server. 
-void serverChat(int sockfd) { 
+void serverChat(int connfd) { 
     char buff[MAX]; 
     int n; 
     // infinite loop for chat 
@@ -27,19 +22,20 @@ void serverChat(int sockfd) {
         bzero(buff, MAX); 
   
         // read the message from client and copy it in buffer 
-        read(sockfd, buff, sizeof(buff)); 
+        read(connfd, buff, sizeof(buff)); 
         // print buffer which contains the client contents 
         printf("From client: %s\t To client : ", buff); 
-        bzero(buff, MAX); 
-        n = 0; 
+       
         // copy server message in the buffer 
-        while ((buff[n++] = getchar()) != '\n') 
+        
+		/*while ((buff[n++] = getchar()) != '\n') 
 		;
-		//cin>>msg;
-  
         // and send that buffer to client 
-        write(sockfd, buff, sizeof(buff)); 
-  
+        write(connfd, buff, sizeof(buff)); */
+		string ack = "received at server\n";
+		bzero(buff, MAX); 
+        n = 0; 
+		write(connfd, (char *)ack.c_str(), ack.length()); 
         // if msg contains "Exit" then server exit and chat ended. 
         if (strncmp("exit", buff, 4) == 0) { 
             printf("Server Exit...\n"); 
@@ -94,49 +90,3 @@ int main() {
     close(sockfd);
 	return 0;
 }
-
-/*
-struct sockaddr_in servaddr, cli; 
-  
-    // socket create and verification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-	//servaddr.sin_addr.s_addr = htonl("192.168.0.11"); 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
-  
-    // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully binded..\n"); 
-  
-    // Now server is ready to listen and verification 
-    if ((listen(sockfd, 5)) != 0) { 
-        printf("Listen failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Server listening..\n"); 
-    len = sizeof(cli); 
-  
-    // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (SA*)&cli, (socklen_t*) &len); 
-    if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("server acccept the client...\n"); 
-*/
